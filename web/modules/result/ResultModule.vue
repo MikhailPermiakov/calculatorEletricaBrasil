4b280e8761901586080d10fcf613a77748ba1652
mikhail <mp9512056251@gmail.com>
2025-05-23 22:17:17 +0300

    8 - уменьшил размер кнопки "установить", добавил кнопку очистить, добавл в результате Cabo cobre.

--- web/modules/result/ResultModule.vue
+++ web/modules/result/ResultModule.vue
@@ -3,7 +3,7 @@
     <h1 v-show="comment" class="text-xl mb-2">Atenção!  Em um circuito monofásico, para usar dispositivos de alta tensão, é necessário um transformador!</h1>
     <p>Demanda máxima: {{ demandaResult }} kVA</p>
     <p class="mt-2">Padrão: {{ padrao?.breaker }}A {{ padrao?.fases }}</p>
-    <p class="mt-2">Cabo:  fase: {{ padrao?.cableDiameter?.fase }}mm2; neutro: {{ padrao?.cableDiameter?.neutro }}mm2;
+    <p class="mt-2">Cabo cobre:  fase: {{ padrao?.cableDiameter?.fase }}mm2; neutro: {{ padrao?.cableDiameter?.neutro }}mm2;
       terra: {{ padrao?.cableDiameter?.terra }}mm2</p>
     <p class="mt-2">Disjuntor geral: {{ geral?.breaker }} A {{ geral?.polarity }}</p>
     <p class="mt-2">IDR: {{ idr?.breaker }}A {{ idr?.polarity }}</p>
